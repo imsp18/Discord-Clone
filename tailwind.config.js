92ebabb691fe90fb77a451a32113fee8786fd5c3
Shivang <shivangparmar537@gmail.com>
2023-04-28 20:10:36 +0530

    Improved the responsiveness of the hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      "blackhans": ['Black Han Sans', 'sans-serif'],
+      "opensans": ['Open Sans', 'sans-serif'],
+    },
     container: {
       center: true,
       padding: {
